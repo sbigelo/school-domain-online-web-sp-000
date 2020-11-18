985522fb792463420401050d6ff6e92e612d0caa
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-18 17:09:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require 'pry'
+class School
+ attr_accessor :name, :roster
+  
+  def initialize(name)
+    
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+   roster[grade] ||= []
+   roster[grade] << student_name
+   
+  end
+  
+  def grade(s_grade)
+    roster[s_grade]
+  
+  end
+  
+end
